1a647bdac8cca7b3b35b1c6352bc47974d0be958
LucasGSFreitas <lucas.silverio014@gmail.com>
2015-07-06 15:45:44 -0300

    Day One-Collection View

---
+++ LazerQuizz/UnlockedCVCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  LazerQuizz
+//
+//  Created by Lucas Gabriel Silvério de Freitas on 06/07/15.
+//  Copyright (c) 2015 LetsPlay. All rights reserved.
+//
+
+import UIKit
+
+class UnlockedCVCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgCell: UIImageView!
+    @IBOutlet weak var lblCell: UILabel!
+}
