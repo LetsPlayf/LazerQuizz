e57d896d9a4a30ff0539acbea09cd1972fc2a25f
Susin Matheus <mathy@Susin-MBPro.local>
2015-07-07 14:15:24 -0300

    Teste

--- LazerQuizz/AppDelegate.swift
+++ LazerQuizz/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  LazerQuizz
-//
-//  Created by Lucas Gabriel Silvério de Freitas on 06/07/15.
-//  Copyright (c) 2015 LetsPlay. All rights reserved.
-//
-
 import UIKit
 import CoreData
 
