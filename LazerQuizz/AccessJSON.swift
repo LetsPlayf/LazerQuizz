5ac7b4cc3593cc06543fd3f8e8564b6b68b9e6b3
ironzep <renangeraldo@MacBook-Pro-de-Renan.local>
2015-07-13 13:47:35 -0300

    Coment

--- LazerQuizz/AccessJSON.swift
+++ LazerQuizz/AccessJSON.swift
@@ -11,61 +11,61 @@ import Foundation
 class AccessJSON {
     
     
+    //This method returns the question that is going to be used. It receives a string that is the area of the question.
     static func  accessTheQuestion(type : String)-> String
     {
+        //The string to be returned.
         var question = [String]()
         
+        //Accessing the JSON.
         let filePath = NSBundle.mainBundle().pathForResource("Perguntas", ofType: "json")
-        //let filePath = NSBundle.mainBundle().pathForResource(arq, ofType: "json")
-        
         var readError : NSError?
-        
         let data = NSData(contentsOfFile: filePath!, options: NSDataReadingOptions.DataReadingUncached,error: &readError)
-        
         let json = JSON(data:data!)
         
+        
+        //Getting all the questions in an area and inserting in an array.
         for (key : String, quest: JSON ) in json["Perguntas"][type]{
             //for (key : String, quest: JSON ) in json[arq][type]{
             question.append(key)
         }
+        
+        //Creating a random variable.
         var indexOfTheQuestion = arc4random_uniform(UInt32(question.count))
         
+        //Returning a random question.
         return question[Int(indexOfTheQuestion)]
     }
     
     
     
-    //This method access the options in of a level
+    //This method accesses the level of the questions and returns the right and left options in the game.
     static func accessTheOptions(type : String, question : String, level : String) -> [String] {
         
         var answers = [String]()
         var arrayOfOptions = [String]()
         
+        
+        //Accessing the JSON
         let filePath = NSBundle.mainBundle().pathForResource("Perguntas", ofType: "json")
-        //let filePath = NSBundle.mainBundle().pathForResource(arq, ofType: "json")
-        
         var readError : NSError?
-        
         let data = NSData(contentsOfFile: filePath!, options: NSDataReadingOptions.DataReadingUncached,error: &readError)
-        
         let json = JSON(data:data!)
         
         for (key : String, quest: JSON ) in json["Perguntas"][type][question][level]{
-            //for (key : String, quest: JSON ) in json[arq][type][question][level]{
+
             answers.append(key)
             
         }
         
+        //Sorting the two options.
         var indexOfTheAnswer = arc4random_uniform(UInt32(answers.count))
-        
         arrayOfOptions.append(answers[Int(indexOfTheAnswer)])
-        
         var indexOfTheAnswer2 = UInt32()
-        
         do {
             indexOfTheAnswer2 = arc4random_uniform(UInt32(answers.count))
-            
         } while(indexOfTheAnswer2 == indexOfTheAnswer)
+        
         
         arrayOfOptions.append(answers[Int(indexOfTheAnswer2)])
         println(arrayOfOptions[0])
@@ -77,22 +77,24 @@ class AccessJSON {
         
     }
     
+    
+    
+    //Method to get the answers. It returns a dictionary with a string and an int.
     static func accessTheAnswers(type : String, question : String, level : String, option1 : String, option2 : String) -> Dictionary<String,Int>{
         
+        //The dictionary that is going to be returned
         var dictionaryOfAnswers = Dictionary<String,Int>()
         
+        //Accessing the JSON
         let filePath = NSBundle.mainBundle().pathForResource("Perguntas", ofType: "json")
-        //let filePath = NSBundle.mainBundle().pathForResource(arq, ofType: "json")
-        
         var readError : NSError?
-        
         let data = NSData(contentsOfFile: filePath!, options: NSDataReadingOptions.DataReadingUncached,error: &readError)
-        
         let json = JSON(data:data!)
         
+        //Auxiliary variables to create the Dictionary that is going to be returned.
         var vet: [Dictionary<String, Int>] = []
-        
         var dic : Dictionary<String,Int>
+        
         
         for (var i = 0; i < json["Perguntas"][type][question][level][option1].count; i++){
             //for (var i = 0; i < json[arq][type][question][level][option1].count; i++){
@@ -117,11 +119,12 @@ class AccessJSON {
         }
         
         
-        
+        //Variable to randomize the selected values to be used in the dictionary.
         var indexOfTheQuestion : Int = Int(arc4random_uniform(UInt32(vet.count)))
         
         var i = 0
         
+        //Getting all the keys and values of the auxiliary variables and putting in the Dictionary
         while (i < 10){
             let key = vet[indexOfTheQuestion].keys
             let value = (vet[indexOfTheQuestion][key.array[0]])
