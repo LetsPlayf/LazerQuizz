db8dae24786a165e5ac03cd0e5fbb290b7752702
ironzep <renangeraldo@MacBook-Pro-de-Renan.local>
2015-08-07 12:44:07 -0300

    To Lucas

--- LazerQuizz/TutorialViewController.swift
+++ LazerQuizz/TutorialViewController.swift
@@ -18,6 +18,8 @@ class TutorialViewController: UIViewController {
     @IBOutlet weak var top2: NSLayoutConstraint!
     
     var tutorialView : UIView?
+    var blurView : UIVisualEffectView?
+    
     
     @IBOutlet weak var view1: UIView!
     @IBOutlet weak var view2: UIView!
@@ -30,29 +32,82 @@ class TutorialViewController: UIViewController {
          top2.constant = CGFloat(1000)
         // Do any additional setup after loading the view.
         
+        self.tutorialView = UILabel(frame: CGRectMake(10, -125, self.view.bounds.width - 20, 125))
+        self.tutorialView!.backgroundColor = UIColor(red: 0.75, green: 0.75, blue: 0.75, alpha: 0.9)
+        self.tutorialView!.layer.masksToBounds = true
+        self.tutorialView!.layer.cornerRadius = 3
+
+
+
+        var blur = UIBlurEffect(style: UIBlurEffectStyle.Light)
+
+        
+        blurView = UIVisualEffectView(effect: blur)
+        
+        
+        blurView!.frame = self.view.bounds
+        
+        self.view.addSubview(blurView!)
+        
         
         
         
     }
 
     
+    @IBAction func tap(sender: AnyObject) {
+
+          self.top.constant = CGFloat(0)
+          self.top2.constant = CGFloat(0)
+        
+        UIView.animateKeyframesWithDuration(2.0, delay: 0.3, options: nil, animations: { () -> Void in
+            
+            UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 0.50, animations: { () -> Void in
+                
+                self.tutorialView?.alpha = 0
+                                self.view.layoutIfNeeded()
+ 
+               
+            })
+            
+            
+            UIView.addKeyframeWithRelativeStartTime(1.0, relativeDuration: 0.25, animations: { () -> Void in
+                
+                self.blurView?.removeFromSuperview()
+
+                
+                
+            })
+
+            
+            
+            
+            UIView.addKeyframeWithRelativeStartTime(1.5, relativeDuration: 0.25, animations: { () -> Void in
+               
+    
+                
+
+            })
+            
+            }) { (Bool) -> Void in
+               
+
+                
+        }
+        
+
+    
+    
+    }
     
     
     override func viewDidAppear(animated: Bool) {
-
-        self.top.constant = CGFloat(0)
-        self.top2.constant = CGFloat(0)
         
+        self.view.addSubview(self.tutorialView!)
         UIView.animateWithDuration(1.0, animations: { () -> Void in
-           
-            self.view.layoutIfNeeded()
-            
-            }, completion: {
-                (value: Bool) in
-               
+            self.tutorialView!.frame.origin.y = self.view.center.y - self.tutorialView!.bounds.height + 10
         })
-
-
+        
         
     }
     override func didReceiveMemoryWarning() {
